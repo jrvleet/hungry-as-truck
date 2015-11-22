78aa1ff79867cf73fde99b527b81407cf5d9ec2e
Alex DeMars <alexdemars@gmail.com>
2015-11-21 20:37:57 -0800

    edit user model, add truck model

---
+++ hungry-as-truck-app/models/truck.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'),
+    debug    = require('debug')('app:models');
+
+var truckSchema = new mongoose.Schema({
+  name: String
+  location: String,
+  duration: String,
+  isActive? : Boolean,
+  category: Boolean,
+  ownerId: String,
+  likes: Array
+});
+
+var Truck = mongoose.model('Truck', truckSchema);
+
+module.exports = Truck;
--- hungry-as-truck-app/models/user.js
+++ hungry-as-truck-app/models/user.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  email:   String,
+  isTruckOwner? : Boolean,
+  trucks: Array
 });
 
 var User = mongoose.model('User', userSchema);
