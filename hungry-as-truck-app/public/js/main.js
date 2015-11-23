1524b881aa7d42098ff898f8a523e99827549926
Alex DeMars <alexdemars@gmail.com>
2015-11-23 14:06:36 -0800

    maps API up and running

--- hungry-as-truck-app/public/js/main.js
+++ hungry-as-truck-app/public/js/main.js
@@ -1,1 +1,9 @@
 console.log('JS loaded!');
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
