7cbbaf75685ebf64985c35e5aa5e716e955a8e71
Alex DeMars <alexdemars@gmail.com>
2015-11-25 17:12:00 -0800

    add input and submit to welcome/index.ejs , edit main.js to update google map with new location from input

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,44 @@
-console.log('JS loaded!');
-
+console.log('Great Job!');
 
 var map;
 var currentLocation;
-function initMap() {
+var geocoder;
+
+var currentLocater = function() {
   if(navigator.geolocation) {
     browserSupportFlag = true;
     navigator.geolocation.getCurrentPosition(function(position) {
       map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: position.coords.latitude, lng: position.coords.longitude},
-        zoom: 14
+        zoom: 16
       });
       currentLocation = new google.maps.Marker({
         position: {lat: position.coords.latitude, lng: position.coords.longitude},
         map: map,
-        title: 'Hello World!'
-  });
-
-    })
+        title: 'You'
+      });
+    });
   }
+};
+
+function initMap() {
+  currentLocater();
+  geocoder = new google.maps.Geocoder;
 }
 
+function codeAddress() {
+  var address = document.getElementById("address").value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      currentLocation = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
 
-
-
-
-
-// if(navigator.geolocation) {
-//   browserSupportFlag = true;
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-//     map.setCenter(initialLocation);
-//   }, function() {
-//     handleNoGeolocation(browserSupportFlag);
-//   });
-// }
+document.getElementById("geocode").addEventListener("click", codeAddress);
