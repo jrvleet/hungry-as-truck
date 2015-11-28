15d26b6f2aebddfd3dfa84121809a7460e092b29
Rob Gonnella <robgonnella@gmail.com>
2015-11-27 17:36:04 -0800

    point at mongolab database instead of local

--- config/database.js
+++ config/database.js
@@ -2,19 +2,19 @@ var mongoose = require('mongoose');
 
 var env = require('./environment');
 
-var dbUri = env.MONGOLAB_URI ||
-            'mongodb://localhost/' + env.SAFE_TITLE;
+// var dbUri = env.MONGOLAB_URI ||
+//             'mongodb://localhost/' + env.SAFE_TITLE;
 
-if (!env.MONGOLAB_URI) {
-  // check that MongoD is running...
-  require('net').connect(27017, 'localhost').on('error', function() {
-    console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
-    process.exit(0);
-  });
-}
+// if (!env.MONGOLAB_URI) {
+//   // check that MongoD is running...
+//   require('net').connect(27017, 'localhost').on('error', function() {
+//     console.log("YOU MUST BOW BEFORE THE MONGOD FIRST, MORTAL!");
+//     process.exit(0);
+//   });
+// }
 
 // Use different database URIs based on whether an env var exists.
-// var dbUri = "mongodb://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@ds057944.mongolab.com:57944/hungry-as-truck"
+var dbUri = "mongodb://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@ds057944.mongolab.com:57944/hungry-as-truck"
 
 // if (!env.MONGOLAB_URI) {
 //   // check that MongoD is running...
