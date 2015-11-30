cb31ed8c26f3cc90eafc30c915445e3e76b8d968
Jessica Vliet <jvliet@gmail.com>
2015-11-29 16:30:09 -0800

    add info bubble to trucks on map

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,10 @@ var truckIcon = './assets/foodTruckIconSM_R.png';
 
 document.getElementById("geocode").addEventListener("click", codeAddress);
 
+
+
+
+
 function initMap() {
   geocoder = new google.maps.Geocoder;
   if(navigator.geolocation) {
@@ -21,6 +25,58 @@ function initMap() {
         position: {lat: position.coords.latitude, lng: position.coords.longitude},
         map: map,
         title: 'You'
+      });
+
+      $.ajax({
+        url: "http://localhost:3000/truckowners",
+        type: "get",
+        data: 'json',
+        success: function (data) {
+          data.forEach(function(truckOwner) {
+            truckOwner.trucks.forEach(function(truck) {
+              var address = truck.location;
+              geocoder.geocode( { 'address': address }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+
+                  var contentString = '<div id="content">'+
+                    '<div id="siteNotice">'+
+                    '</div>'+
+                    '<h1 id="firstHeading" class="firstHeading">'+ truck.name +'</h1>'+
+                    '<div id="bodyContent">'+
+                    '<p>' + truck.location + '</p>'
+                    '</div>'+
+                    '</div>';
+
+                    var infowindow = new google.maps.InfoWindow({
+                      content: contentString
+                    });
+
+                    var marker = new google.maps.Marker({
+                      map: map,
+                      position: results[0].geometry.location,
+                      icon: truckIcon
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                      infowindow.close(map, marker);
+                      infowindow.open(map, marker);
+                    });
+
+                    // marker.addListener('click', function() {
+                    //   infowindow.open(map, marker);
+                    // });
+                }
+
+                else {
+                  alert("Geocode was not successful for the following reason: " + status);
+                }
+              });
+            })
+          })
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          alert("Status: " + textStatus + "    Error:" + errorThrown);
+        }
       });
     });
   }
@@ -41,34 +97,3 @@ function codeAddress() {
     }
   });
 }
-
-$.ajax({
-  url: "http://localhost:3000/truckowners",
-  type: "get",
-  data: 'json',
-  success: function (data) {
-    data.forEach(function(truckOwner) {
-      truckOwner.trucks.forEach(function(truck) {
-        var address = truck.location;
-        geocoder.geocode( { 'address': address }, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location,
-              icon: truckIcon
-            });
-          }
-          else {
-            alert("Geocode was not successful for the following reason: " + status);
-          }
-        });
-      })
-    })
-  },
-
-  
-
-  error: function (XMLHttpRequest, textStatus, errorThrown) {
-    alert("Status: " + textStatus + "    Error:" + errorThrown);
-  }
-});
